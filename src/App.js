7486bba14753ca635fb43cb525eccf063a8a2e1a
ratchav <ratcha.keunma@gmail.com>
2024-07-25 13:52:59 +0700

    add axios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useEffect, useRef, useState } from 'react';
+
+const fetchData = async () => {
+  const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
+  return response.data;
+};
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const count = useRef(0);
+  const url = "https://jsonplaceholder.typicode.com/users";
+  const [data, setData] = useState([]);
+  const fetchInfo = () => {
+    return fetch(url)
+      .then((res) => res.json())
+      .then((d) => setData(d))
+  }
+
+
+  useEffect(() => {
+    fetchInfo();
+  }, []);
+  
+  useEffect(() => {
+    count.current = count.current + 1;
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="App">
+      <h1 style={{ color: "green" }}>using JavaScript inbuilt FETCH API</h1>
+      <center>
+        {data.map((dataObj, index) => {
+          return (
+            <div
+              style={{
+                width: "15em",
+                backgroundColor: "#35D841",
+                padding: 2,
+                borderRadius: 10,
+                marginBlock: 10,
+              }}
+            >
+              <p style={{ fontSize: 20, color: 'white' }}>{dataObj.name}</p>
+            </div>
+          );
+        })}
+      </center>
+    </div>
+      <input
+        type='text'
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <h1>Reder Count: {count.current}</h1>
     </div>
   );
 }
